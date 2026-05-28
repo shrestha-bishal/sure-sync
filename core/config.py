4b9235ade37fc13dfc4e3d557b6823facd8cf7e1
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2026-05-28 20:57:26 +1000

    cleaned up the reduntant volume path variable

--- core/config.py
+++ core/config.py
@@ -1,10 +1,9 @@
 import os
 
-CONSUME_PATH = "/app/consume"
+CONSUME_PATH = os.getenv("CONSUME_PATH", "/app/consume")
 PROCESSED_DIR = os.path.join(CONSUME_PATH, "processed")
 FAILED_DIR = os.path.join(CONSUME_PATH, "failed")
 
-VOLUME_CONSUME_PATH = os.getenv("CONSUME_PATH", CONSUME_PATH)
 LOOKUP_INTERVAL = int(os.getenv("LOOKUP_INTERVAL", "5")) # default 5 seconds
 
 API_URL = os.getenv("API_URL", None)
