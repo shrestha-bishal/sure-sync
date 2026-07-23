68ccbf425f3713712ffaed6f94bcf1a3f97d99ed
shrestha-bishal <bishal.shrestha@outlook.com.au>
2026-07-23 15:24:36 +1000

    streamlined the mapping data and removed the repetative code

--- worker/main.py
+++ worker/main.py
@@ -78,50 +78,68 @@ stats.save(os.path.join(DATA_PATH, "stats.json"))
 # Consuming
 def process_file(file_path):
     file_name = os.path.basename(file_path)
-    log(f"Processing file: {file_name}")
-
-    parsed_data = parser.parse(file_path)
-    log(f"Parsed data from {file_name}")
+    log(f"Processing file: {file_name}") 
 
     account_name = None
-    bank_name = None
-    transaction_dates = [
-        data.get("date")
-        for data in parsed_data
-    ]
-
-    from_date = min(transaction_dates)
-    to_date = max(transaction_dates)
-
-    account_ids = {data.get("account_id") for data in parsed_data}
-
-    existing_transactions = api_client.get_transactions_by_date(
-        from_date=from_date,
-        to_date=to_date,
-        account_ids=list(account_ids)
-    )
-
-    log(existing_transactions)
+    bank_name = None 
 
     # Processing the data
     try:
+        parsed_data = parser.parse(file_path)
+        log(f"Parsed data from {file_name}")
+
+        if not parsed_data:
+            raise ValueError("No transactions found")
+
+        mapped_transactions = []
+
         for data in parsed_data:
             bank_id = data.get("bank_id")
             account_id = data.get("account_id")
+
             key = f"{bank_id}:{account_id}"
             mapping = valid_mappings.get(key)
 
             if not mapping:
                 log(f"Account {key} not mapped. Skipping.")
-                raise ValueError(f"Account {key} is not mapped.")
-            
-            sure_account_id = mapping.sure_account_id
+                raise ValueError(f"Account {key} is not mapped")
+
+
+            mapped_transactions.append({
+                "data": data,
+                "mapping": mapping
+            })
+
+        transaction_dates = [
+            item["data"].get("date")
+            for item in mapped_transactions
+        ]
+
+        from_date = min(transaction_dates)
+        to_date = max(transaction_dates)
+
+        sure_account_ids = list({
+            item["mapping"].sure_account_id
+            for item in mapped_transactions
+        })
+
+        existing_transactions = api_client.get_transactions_by_date(
+            from_date=from_date,
+            to_date=to_date,
+            account_ids=list(sure_account_ids)
+        )
+
+        log(existing_transactions)
+
+        for item in mapped_transactions:
+            data = item["data"]
+            mapping = item["mapping"]
+
             account_name = mapping.account_name
             bank_name = mapping.bank_name
-            transaction_dates.append(data.get("date"))
 
             transaction = Transaction.from_ofx_data(
-                sure_account_id=sure_account_id,
+                sure_account_id=mapping.sure_account_id,
                 data=data)
 
             result = api_client.create_transaction(transaction=transaction)
